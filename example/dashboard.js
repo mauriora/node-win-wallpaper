a8dca0e49a4f85621472d21744eb91105b9087f6
DennisKuhn <dennis@kuhn.geek.nz>
2020-03-31 21:33:14 +1300

    Title and color in page query parameter

--- example/dashboard.js
+++ example/dashboard.js
@@ -5,15 +5,37 @@ const downBar = document.getElementById('networkSpeed_downBar');
 const upValue = document.getElementById('networkSpeed_upValue');
 const upBar = document.getElementById('networkSpeed_upBar');
 
-setInterval(function() {
-  const speedDown = Math.floor(Math.random() * 1000);
+setInterval(function () {
+	const speedDown = Math.floor(Math.random() * 1000);
 
-  downValue.innerText = speedDown;
-  downBar.style.width = speedDown / 10 + '%';
+	downValue.innerText = speedDown;
+	downBar.style.width = speedDown / 10 + '%';
 
-  const speedUp = Math.floor(Math.random() * 1000);
+	const speedUp = Math.floor(Math.random() * 1000);
 
-  upValue.innerText = speedUp;
-  upBar.style.width = speedUp / 10 + '%';
+	upValue.innerText = speedUp;
+	upBar.style.width = speedUp / 10 + '%';
 }, 300);
 
+const getQueryVariable = function (variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		if (decodeURIComponent(pair[0]) == variable) {
+			return decodeURIComponent(pair[1]);
+		}
+	}
+};
+
+const title = getQueryVariable('title');
+const backgroundColor = getQueryVariable('background-color');
+
+console.log(title, backgroundColor);
+
+if (title) {
+	document.querySelector('h1').innerText = title;
+}
+if (backgroundColor) {
+	document.querySelector('body').style.backgroundColor = backgroundColor;
+}
